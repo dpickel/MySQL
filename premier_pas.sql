81a3a5937c8cc154a844b9c882f35616f71dd492
unknown <eliole@hotmail.fr>
2017-08-22 12:56:04 +0200

    Fin premier tp

--- premier_pas.sql
+++ premier_pas.sql
@@ -47,7 +47,7 @@ INSERT INTO base_restaurant.Ingredient ( nom_ingredient ) VALUES ( 'Boeuf' );
 INSERT INTO base_restaurant.Ingredient ( nom_ingredient ) VALUES ( 'Lardon' );
 INSERT INTO base_restaurant.Ingredient ( nom_ingredient ) VALUES ( 'Oeuf' );
 INSERT INTO base_restaurant.Ingredient ( nom_ingredient ) VALUES ( 'Pate' );
-INSERT INTO base_restaurant.Ingredient ( nom_ingredient ) VALUES ( 'Champignons' );
+INSERT INTO base_restaurant.Ingredient ( nom_ingredient ) VALUES ( 'Champignon' );
 INSERT INTO base_restaurant.Ingredient ( nom_ingredient ) VALUES ( 'Crême fraîche' );
 
 INSERT INTO base_restaurant.plat ( nom ) VALUES ( 'Pates carbonara' );
@@ -63,7 +63,58 @@ INSERT INTO base_restaurant.commande ( nom, la_date, total) VALUES ( 'Mr Martin'
 INSERT INTO base_restaurant.commande ( nom, la_date, total) VALUES ( 'Mr Dupont', 12/10/2007, 108.6 );
 INSERT INTO base_restaurant.commande ( nom, la_date, total) VALUES ( 'Mme Dupont', 14/10/2007, 42 );
 
+-- Création des recettes
+
+INSERT INTO base_restaurant.plat_ingredients ( id_plat, nom_ingredient , quantite ) 
+	VALUES (
+    (select id_plat from base_restaurant.plat where nom='Pates carbonara'),'Pate', 125),
+    ((select id_plat from base_restaurant.plat where nom='Pates carbonara'), 'Lardon', 100),
+    ((select id_plat from base_restaurant.plat where nom='Pates carbonara'), 'Oignon', 50),
+    ((select id_plat from base_restaurant.plat where nom='Pates carbonara'), 'Oeuf', 1)
+    ;
+    
+    INSERT INTO base_restaurant.plat_ingredients ( id_plat, nom_ingredient , quantite ) 
+	VALUES (
+    (select id_plat from base_restaurant.plat where nom='Salade paysanne'),'Salade verte', 1),
+    ((select id_plat from base_restaurant.plat where nom='Salade paysanne'), 'Lardon', 100),
+    ((select id_plat from base_restaurant.plat where nom='Salade paysanne'), 'Patate', 50)
+    ;
+
+    INSERT INTO base_restaurant.plat_ingredients ( id_plat, nom_ingredient , quantite ) 
+	VALUES (
+    (select id_plat from base_restaurant.plat where nom='Steack frites'),'Boeuf', 100),
+    ((select id_plat from base_restaurant.plat where nom='Steack frites'), 'Patate', 100)
+    ;
+    
+	INSERT INTO base_restaurant.plat_ingredients ( id_plat, nom_ingredient , quantite ) 
+	VALUES (
+    (select id_plat from base_restaurant.plat where nom='frites'),'Patate', 100)
+    ;
+    
+	INSERT INTO base_restaurant.plat_ingredients ( id_plat, nom_ingredient , quantite ) 
+	VALUES (
+    (select id_plat from base_restaurant.plat where nom='Omelette au poivron'),'Oeuf', 2),
+    ((select id_plat from base_restaurant.plat where nom='Omelette au poivron'), 'Crême fraîche', 10),
+    ((select id_plat from base_restaurant.plat where nom='Omelette au poivron'), 'Poivron', 1)
+    ;
+    
+	INSERT INTO base_restaurant.plat_ingredients ( id_plat, nom_ingredient , quantite ) 
+	VALUES ((select id_plat from base_restaurant.plat where nom='Risotto'),'Riz', 75),
+    ((select id_plat from base_restaurant.plat where nom='Risotto'), 'Crême fraîche', 50),
+    ((select id_plat from base_restaurant.plat where nom='Risotto'), 'Champignon', 50),
+    ((select id_plat from base_restaurant.plat where nom='Risotto'), 'Poivron', 1)
+    ;
 
 
+-- Création du contenu des commandes
 
+	INSERT INTO base_restaurant.Contenu_commande ( id_commande, id_plat ) 
+	VALUES (
+    (select id_commande from base_restaurant.Commande where nom='Mme Dupont'),
+    (select id_plat from base_restaurant.plat where nom='Risotto')
+    );
 
+	INSERT INTO base_restaurant.Contenu_commande ( id_commande, id_plat ) 
+	VALUES 
+    ((select id_commande from base_restaurant.Commande where nom='Mme Titi'), (select id_plat from base_restaurant.plat where nom='Omelette au poivron')),
+	((select id_commande from base_restaurant.Commande where nom='Mme Titi'), (select id_plat from base_restaurant.plat where nom='Frites'));
